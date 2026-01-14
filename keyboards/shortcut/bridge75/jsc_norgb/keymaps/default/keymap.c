f0648fcd864ae37dbcaf9b9861a4a5ae591a6bb4
RasyaJusticio <rasyajusticio.public@gmail.com>
2026-01-14 10:25:51 +0700

    feat: create and configure a personalize profile

---
+++ keyboards/shortcut/bridge75/jsc_norgb/keymaps/default/keymap.c
@@ -0,0 +1,51 @@
+// Copyright 2024 Wind (@yelishang)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    PERSONAL_B,
+    PERSONAL_FN,
+    PUBLIC_B,
+    PUBLIC_FN,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [PERSONAL_B] = LAYOUT_ansi(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, _______, KC_END,
+        KC_CAPS, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(PERSONAL_FN),  _______, _______, _______
+    ),
+
+    [PERSONAL_FN] = LAYOUT_ansi(
+        _______, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PSCR,                   KC_INS,
+        BT_USB,  LT(0, BT_HOST1), LT(0, BT_HOST2), LT(0, BT_HOST3), LT(0, BT_2_4G), _______, _______, _______, _______, _______, _______, _______, _______, _______, PDF(PUBLIC_B),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, EE_CLR,
+        _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______
+    ),
+
+    [PUBLIC_B] = LAYOUT_ansi(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(PUBLIC_FN),    KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [PUBLIC_FN] = LAYOUT_ansi(
+        _______, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PSCR,                   KC_INS,
+        BT_USB,  LT(0, BT_HOST1), LT(0, BT_HOST2), LT(0, BT_HOST3), LT(0, BT_2_4G), _______, _______, _______, _______, _______, _______, _______, _______, _______, PDF(PERSONAL_B),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, EE_CLR,
+        _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______
+    )
+};
+// clang-format on
